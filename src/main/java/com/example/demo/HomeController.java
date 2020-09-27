a29f4fa0286ea81fcbf4f89b6e76e9bdd32f3d6c
craigh210 <craighunt210@gmail.com>
2020-09-26 23:43:59 -0400

    Version 405

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,7 +1,11 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 public class HomeController {
@@ -19,8 +23,13 @@ public class HomeController {
         return "admin";
     }
 
+    @Autowired
+    UserRepository userRepository;
+
     @RequestMapping("/secure")
-    public String secure() {
+    public String secure(Principal principal, Model model) {
+        String username = principal.getName();
+        model.addAttribute("user", userRepository.findByUsername(username));
         return "secure";
     }
 }
